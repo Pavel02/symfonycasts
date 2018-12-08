f629e32d9d02007862eb8edda35b8775d9fc8951
pavel02 <pavel.hr@mail.ru>
2018-12-09 02:30:16 +0300

    added names routes

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,15 +10,15 @@ use Symfony\Component\HttpFoundation\Response;
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
-        return new Response('OMG! My first page already! WOOOO!');
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug)
     {
@@ -28,7 +28,7 @@ class ArticleController extends AbstractController
             'I like bacon too! Buy some from my site! bakinsomebacon.com',
         ];
 
-        dump($slug, $this);
+        //dump($slug, $this);
 
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
