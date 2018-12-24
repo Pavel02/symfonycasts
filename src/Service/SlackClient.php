0152889e65e2dc0ecebaa7d273dfb7af4c00b385
pavel02 <pavel.hr@mail.ru>
2018-12-24 20:08:48 +0300

    added service SlackClient

---
+++ src/Service/SlackClient.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Service;
+
+use Nexy\Slack\Client;
+use Psr\Log\LoggerInterface;
+
+class SlackClient
+{
+    /**
+     * @var Client
+     */
+    private $slack;
+
+    public function __construct(Client $slack)
+    {
+        $this->slack = $slack;
+    }
+
+    public function sendMessage(string $from, string $message)
+    {
+        $message = $this->slack->createMessage()
+            ->from($from)
+            ->withIcon(':ghost:')
+            ->setText($message);
+        $this->slack->sendMessage($message);
+    }
+}
