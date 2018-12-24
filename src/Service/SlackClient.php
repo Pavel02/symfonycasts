ef8ff86d50b09df9dae899812b4846b569dbfa02
pavel02 <pavel.hr@mail.ru>
2018-12-24 21:10:56 +0300

    added LoggerTrait

--- src/Service/SlackClient.php
+++ src/Service/SlackClient.php
@@ -2,37 +2,30 @@
 
 namespace App\Service;
 
+use App\Helper\LoggerTrait;
 use Nexy\Slack\Client;
 use Psr\Log\LoggerInterface;
 
 class SlackClient
 {
+    use LoggerTrait;
+
     /**
      * @var Client
      */
     private $slack;
-    /**
-     * @var LoggerInterface|null
-     */
-    private $logger;
 
     public function __construct(Client $slack)
     {
         $this->slack = $slack;
     }
 
-    /**
-     * @required
-     */
-    public function setLogger(LoggerInterface $logger)
-    {
-        $this->logger = $logger;
-    }
-
     public function sendMessage(string $from, string $message)
     {
         if ($this->logger) {
-            $this->logger->info('Beaming a message to Slack!');
+            $this->logger->info('Beaming a message to Slack!', [
+                'message' => $message
+            ]);
         }
 
         $message = $this->slack->createMessage()
