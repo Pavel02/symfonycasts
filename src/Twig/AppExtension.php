08d6bb37e9ed40885ba43b8b117b253befa9e9a9
pavel02 <pavel.hr@mail.ru>
2018-12-30 15:07:35 +0300

    AppExtension  implements  ServiceSubscriberInterface

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -3,17 +3,19 @@
 namespace App\Twig;
 
 use App\Service\MarkdownHelper;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\DependencyInjection\ServiceSubscriberInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 use Twig\TwigFunction;
 
-class AppExtension extends AbstractExtension
+class AppExtension extends AbstractExtension implements ServiceSubscriberInterface
 {
-    private $helper;
+    private $container;
 
-    public function __construct(MarkdownHelper $helper)
+    public function __construct(ContainerInterface $container)
     {
-        $this->helper = $helper;
+        $this->container = $container;
     }
 
     public function getFilters(): array
@@ -29,6 +31,34 @@ class AppExtension extends AbstractExtension
     public function processMarkdown($value)
     {
 //        return strtoupper($value);
-        return $this->helper->parse($value);
+//        return $this->helper->parse($value);
+        return $this->container
+            ->get(MarkdownHelper::class)
+            ->parse($value);
+    }
+
+    /**
+     * Returns an array of service types required by such instances, optionally keyed by the service names used internally.
+     *
+     * For mandatory dependencies:
+     *
+     *  * array('logger' => 'Psr\Log\LoggerInterface') means the objects use the "logger" name
+     *    internally to fetch a service which must implement Psr\Log\LoggerInterface.
+     *  * array('Psr\Log\LoggerInterface') is a shortcut for
+     *  * array('Psr\Log\LoggerInterface' => 'Psr\Log\LoggerInterface')
+     *
+     * otherwise:
+     *
+     *  * array('logger' => '?Psr\Log\LoggerInterface') denotes an optional dependency
+     *  * array('?Psr\Log\LoggerInterface') is a shortcut for
+     *  * array('Psr\Log\LoggerInterface' => '?Psr\Log\LoggerInterface')
+     *
+     * @return array The required service types, optionally keyed by service names
+     */
+    public static function getSubscribedServices()
+    {
+        return [
+            MarkdownHelper::class,
+        ];
     }
 }
